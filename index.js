e0f4426b828f8f5a3db56f516adb99333b610aeb
Alexis Arando <lex4736@gmail.com>
2021-02-04 23:13:54 -0800

    added code to access necessary tools and paths

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 
+// Here is where we will access our tools, technologies and paths 
+const fs = require("fs")
+const path = require("path")
+const inquirer = require("inquirer")
+const axios = require("axios")
+const generateMarkdown = require("./utils/generateMarkdown")
+
 
 // array of questions for user
 const questions = [
